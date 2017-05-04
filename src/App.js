1ba55d5fe2bacf9094b5e632cdb04a6356d5570b
alan moir <mills3@gmx,co.uk>
2017-05-04 13:47:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,36 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 const marked = require('marked');
+const example = `# *Mark* ~~Down~~\n---\n## Example\n *Italic*, **bold**, and \`monospace\`.`;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      example: `# *Mark* ~~Down~~\n---\n## Example\n *Italic*, **bold**, and \`monospace\`.
-                \nItemized lists look like:
-                * this one
-                * that one
-                * the other one`,
+      exampleText: example
     }
   }
 
   handleChange = (e) => {
     this.setState({
-      example: e.target.value
+      exampleText: e.target.value
     });
   }
 
   markup() {
-    const md = this.state.example;
+    const md = this.state.exampleText;
     return {__html: marked(md)};
+  }
+
+  clearExample = () => {
+    this.setState({exampleText: ''})
+  }
+
+  resetExample = () => {
+    this.setState({exampleText: example})
   }
 
   render() {
     return (
       <div className="app">
         <h1 className="title">Markdown Previewer</h1>
-        <textarea className="input areas" onChange={this.handleChange} value={this.state.example}></textarea>
+        <div className="btnsContainer">
+          <button className="clearBtn" onClick={this.clearExample}>Clear</button>
+          <button className="resetBtn" onClick={this.resetExample}>Reset</button>
+        </div>
+        <textarea className="input areas" onChange={this.handleChange} value={this.state.exampleText}></textarea>
         <div className="output areas" dangerouslySetInnerHTML={this.markup()}></div>
+        <a className="githubLink" href="https://github.com/mills3/markdown-previewer" target="_blank">
+          <img className="profileImage" src={require('./images/profileImage.png')} alt="" />
+          See the code on Github.
+        </a>
       </div>
     );
   }
